6fbeef692df5c582434845628fe8690528d75995
umutcan9821 <mahmutcavur@gmail.com>
2024-12-27 22:45:59 -0700

    add slf4 logger & synchronize reading from tcp

--- src/main/java/tr/com/minesoft/minetrack/model/DataTerminal.java
+++ src/main/java/tr/com/minesoft/minetrack/model/DataTerminal.java
@@ -13,7 +13,7 @@ import tr.com.minesoft.minetrack.logging.util.ExceptionToString;
 import tr.com.minesoft.minetrack.model.lists.SignalList;
 
 public class DataTerminal {
-    private static final String IP = "192.168.0.7";
+    private static final String IP = "192.168.1.2";
     private static final int PORT = 4001;
     private volatile boolean state;
     private DataInputStream input;
--- src/main/java/tr/com/minesoft/minetrack/threads/ReadService.java
+++ src/main/java/tr/com/minesoft/minetrack/threads/ReadService.java
@@ -1,13 +1,27 @@
 package tr.com.minesoft.minetrack.threads;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tr.com.minesoft.minetrack.model.DataTerminal;
 
 public class ReadService implements Runnable {
+
+	private final Logger logger = LoggerFactory.getLogger(ReadService.class);
+	private volatile boolean running = false;
+
 	@Override
 	public void run() {
 		while (DataTerminal.getInstance().getState()) {
-			DataTerminal.getInstance().read();
+			try {
+				while (!running) {
+					DataTerminal.getInstance().read();
+					running = true;
+				}
+			}catch (Exception e) {
+				logger.error(e.getMessage());
+			}finally {
+				running = false;
+			}
 		}
 	}
-
 }
